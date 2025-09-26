4dc1df584f0afde481e6f071cf756f2a96c56de0
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-26 18:06:31 -0400

    twosum

---
+++ twosum.py
@@ -0,0 +1,9 @@
+class Solution():
+    def twosum(nums, target):
+        store = {}
+
+        for x, num in enumerate(nums):
+            diff = target - num
+            if diff in store:
+                return [store[diff], x]
+            store[num] = x
