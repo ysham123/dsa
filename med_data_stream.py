403893cf2a257b0efc02fb1c5a718508557a29a8
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-26 17:18:10 -0400

    med_data_stream.py fixed

--- med_data_stream.py
+++ med_data_stream.py
@@ -1,19 +1,22 @@
-#if length is odd, median is middle number, if even , med is average of two middle numbers
 import heapq
 
-class median_finder():
+class MedianFinder:
+
     def __init__(self):
-        self.small = []
-        self.large = []
+        self.small = []  # max heap (store as negatives)
+        self.large = []  # min heap
 
-    def add_num(self, num):
-        heapq.push(self.small, -num)
-        heapq.push(self.large, -heapq.heappop(self.small))
+    def addNum(self, num: int) -> None:
+        # push to max heap
+        heapq.heappush(self.small, -num)
+        # balance by moving the largest from small to large
+        heapq.heappush(self.large, -heapq.heappop(self.small))
 
+        # maintain size property
         if len(self.large) > len(self.small):
             heapq.heappush(self.small, -heapq.heappop(self.large))
 
-    def findmedian(self):
+    def findMedian(self) -> float:
         if len(self.small) > len(self.large):
             return -self.small[0]
-        return (-self.small[0] + self.large[0] // 2)
+        return (-self.small[0] + self.large[0]) / 2
