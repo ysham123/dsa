d6fe639b58d4511e1a0b1bb8176e6f9f4ea79fac
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-26 16:46:57 -0400

    Initial commit: Add meeting rooms II solution

---
+++ meeting_rooms2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minmeetingrooms(self, intervals):
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+        res,count = 0,0
+        s,e = 0,0
+
+        while s < len(intervals):
+            if start[s] < end[e]:
+                s += 1
+                count += 1
+            else:
+                e += 1
+                count += 1
+            res = max(res, count)
+        return res
