377a1dc8bcdd5db9dc1998c5abc82c4adf5eed79
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-27 19:15:01 -0400

    longest_substring_without_repeating_characters.py

---
+++ longest_substring_without_repeating_characters.py
@@ -0,0 +1,17 @@
+from re import L
+
+
+class Solution():
+    def lengthoflongestsubstring(s):
+        res = 0
+        l = 0
+        track = set()
+
+
+        for r in range(len(s)):
+            while s[r] in track:
+                track.remove(s[l])
+                l += 1
+            track.add(s[r])
+            res = max(res, r-l + 1)
+        return res
