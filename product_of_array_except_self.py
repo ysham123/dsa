f6f77f783daca2f49bfe7af0946123b7bb9bbe56
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-27 16:17:57 -0400

    additional useful comments

--- product_of_array_except_self.py
+++ product_of_array_except_self.py
@@ -1,3 +1,6 @@
+from sys import prefix
+
+
 class Solution():
     def productExceptSelf(self, nums):
         res = [1] * (len(nums))
@@ -10,3 +13,28 @@ class Solution():
             res[x] *= postfix
             postfix *= nums[x]
         return res
+
+    #we want each res[i] to hold the product of all elemetns before index i 
+
+    nums = [1,2,3,4]
+    res = [1,1,1,1]
+    prefix = 1
+    #forward pass
+    for i in range(len(nums)):
+        res[i] = prefix
+        prefix *= nums[i]
+
+    # now we need to multiply by the product of all elements after index i
+
+    postfix = 1
+    #backward pass
+
+    for i in range(len(nums)-1,-1,-1):
+        res[i] *= postfix
+        postfix *= nums[i]
+
+    """
+    the forward loop fills in product of elemtns before i
+    the backward loop multiplies in products of elemnts after i
+    together each index gets everything except self
+    """
