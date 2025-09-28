075cc442a9b40c670b23843d5fb175ca356b6b57
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-28 17:04:40 -0400

    a little bit of a better solution, still not optimal

--- threesum.py
+++ threesum.py
@@ -11,4 +11,12 @@ class Solution():
                             res.append(triplets)
         return res
 
-        
+        res = set()
+        nums.sort()
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                for k in range(j+1, len(nums)):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        tmp = sorted[nums[i], nums[j], nums[k]]
+                        res.add(tuple(tmp))
+        return [list(i) for i in res]
