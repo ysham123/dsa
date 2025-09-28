69490b1fd9458af88f0ac32d567676b1c6bd4601
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-27 22:20:38 -0400

    removed duplicates

--- twosum.py
+++ twosum.py
@@ -9,11 +9,3 @@ class Solution():
             store[num] = x
 
 
-class Solution():
-    def twosum(nums, target):
-        prevmap = {}
-        for x, num in enumerate(nums):
-            diff = target - num
-            if diff in prevmap:
-                return [prevmap[diff], x]
-            prevmap[num] = x
