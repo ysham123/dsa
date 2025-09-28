443ba12b77bc7e14a587591bb51fa0572ce39f12
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-28 17:21:51 -0400

    optimal code for threesum

--- threesum.py
+++ threesum.py
@@ -1,6 +1,6 @@
 class Solution():
     def threesum(self, nums:List[int]) -> List[List[int]]:
-        res = []
+        """res = []
 
         for x in range(len(nums)):
             for j in range(x+1, len(nums)):
@@ -19,4 +19,30 @@ class Solution():
                     if nums[i] + nums[j] + nums[k] == 0:
                         tmp = sorted[nums[i], nums[j], nums[k]]
                         res.add(tuple(tmp))
-        return [list(i) for i in res]
+        return [list(i) for i in res]"""
+
+
+        #optimal
+
+        res = []
+        nums.sort()
+
+        for i,a in enumerate(nums):
+            if a > 0:
+                break
+            if i > 0 and a == nums[i-1]:
+                continue 
+            l,r = i+1, len(nums) - 1
+            while l < r:
+                threesum = a + nums[l] + nums[r]
+                if threesum > 0:
+                    r -= 1
+                elif threesum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+        return res
