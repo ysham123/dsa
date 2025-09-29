90b688a83ebf232fd9f19a7bce70a765ed6c447d
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-28 22:38:09 -0400

    added the time and space for the brute force

--- top_k_freq_elements.py
+++ top_k_freq_elements.py
@@ -1,5 +1,8 @@
 class Solution():
     def topkfrequent(self, nums:List[int], k:int) -> List[int]:
-        from collections import Counter
+        """from collections import Counter
         counter = Counter(nums)
-        return[num for num, freq in counter.most_common(k)]
+        return[num for num, freq in counter.most_common(k)]""" #time: Time: O(n + u log k), Space:O(u)
+
+        #The better approach
+
