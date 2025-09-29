142c205809afe677956a9427868d84afc1875c9b
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-29 13:41:16 -0400

    group_anagrams.py

---
+++ group_anagrams.py
@@ -0,0 +1,7 @@
+class Solution():
+    def group_anagrams(self, strs:List[str]) -> List[List[str]]:
+        store = defaultdict(list)
+        for word in strs:
+            key = "".join(sorted(word))
+            store[key].append(word)
+        return list(store.values())
