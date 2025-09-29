4c3c5c9b557f4739d3a8ff9fe91a09df3405de6f
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-28 22:31:20 -0400

    brute force approach

---
+++ top_k_freq_elements.py
@@ -0,0 +1,5 @@
+class Solution():
+    def topkfrequent(self, nums:List[int], k:int) -> List[int]:
+        from collections import Counter
+        counter = Counter(nums)
+        return[num for num, freq in counter.most_common(k)]
