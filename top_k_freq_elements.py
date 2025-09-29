6359bec5f27d2ac5131f9a8685b1d4f0b1d81564
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-28 22:46:39 -0400

    optimal approach with bucket sort

--- top_k_freq_elements.py
+++ top_k_freq_elements.py
@@ -5,4 +5,13 @@ class Solution():
         return[num for num, freq in counter.most_common(k)]""" #time: Time: O(n + u log k), Space:O(u)
 
         #The better approach
-
+        freq_map = Counter(nums)
+        buckets = [[] for _ in range(len(nums) + 1)]
+        for num, freq in freq_map.items():
+            bucket[freq].append(num)
+        res = []
+        for freq in range(len(buckets) -1, 0, -1):
+            for num in bucket[freq]:
+                res.append(num)
+                if len(res) == k:
+                    return res      #time:O(n), space:O(n)
