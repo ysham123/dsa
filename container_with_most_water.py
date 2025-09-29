af68c3a1177eb8eebcea4515bb48c5cf2c6d071e
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-29 13:47:51 -0400

    container with most water solution

---
+++ container_with_most_water.py
@@ -0,0 +1,14 @@
+class Solution():
+    def containter_with_most_water(self,heights:List[int]) -> int:
+        res = 0
+        l,r = 0, len(heights) - 1
+
+        while l < r:
+            area = (heights[l], heights[r]) * (r-l)
+            res = max(res, area)
+
+            if heights[l] < heights[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
