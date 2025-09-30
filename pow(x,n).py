138c1496323f0bdb8213132b2adfd338c9805e93
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-30 10:47:17 -0400

    pow(x,n).py first solution

--- pow(x,n).py
+++ pow(x,n).py
@@ -1,3 +1,11 @@
 class Solution():
     def mypow(self, x:float, n:int) -> float:
-        ''
+        if n == 0:
+            return 1
+        if n < 0:
+            return 1 / self.mypow(x,-n)
+        half = self.mypow(x, n//2)
+        if n % 2 == 0:
+            return half * half
+        else:
+            return half * half * x
