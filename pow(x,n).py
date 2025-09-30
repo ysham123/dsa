4fb830476993a5f0e14271b17a6c4034d1c0fe49
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-30 10:48:59 -0400

    pow(x,n).py iterative solution

--- pow(x,n).py
+++ pow(x,n).py
@@ -1,6 +1,6 @@
 class Solution():
     def mypow(self, x:float, n:int) -> float:
-        if n == 0:
+        """if n == 0:
             return 1
         if n < 0:
             return 1 / self.mypow(x,-n)
@@ -8,4 +8,18 @@ class Solution():
         if n % 2 == 0:
             return half * half
         else:
-            return half * half * x
+            return half * half * x"""
+        #iterative solution, better O(1) space
+        if n < 0:
+            x = 1/x
+            n = -n
+
+        res = 1
+        while n > 0:
+            if n % 2 == 1:
+                res  *= x
+            x *= x
+            n //= 2
+        return res
+
+        
