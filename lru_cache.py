e7cae4d41096788d4ff004feba84e67d299df7ec
Yosef Shammout <YosefShammout123@gmail.com>
2025-10-06 23:57:47 -0400

    simpler approach same time and space lru cache

--- lru_cache.py
+++ lru_cache.py
@@ -1,4 +1,4 @@
-class Node:
+"""class Node:
     def __init__(self, key, val):
         self.key, self.val = key, val
         self.prev = self.next = None
@@ -35,4 +35,24 @@ class LRUCache:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
-#time:o(1), space:o(n)
+#time:o(1), space:o(n)"""
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.cap = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+
+        if len(self.cache) > self.cap:
+            self.cache.popitem(last=False)
