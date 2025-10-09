46dde43f870ed85b7ac2525e68b852444bb7dfcc
Yosef Shammout <YosefShammout123@gmail.com>
2025-10-09 15:32:18 -0400

    second daily recap is LRU cache

--- daily_recap.py
+++ daily_recap.py
@@ -1,3 +1,6 @@
+from typing import OrderedDict
+
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows,cols = len(grid), len(grid[0])
@@ -19,3 +22,23 @@ class Solution:
                     dfs(r,c)
                     count += 1
         return count
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.cap = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+
+        if len(self.cache) > self.cap:
+            self.cache.popitem(last=False)
