5bb3938b7977de33af93ca1262d80bb16b844735
Yosef Shammout <YosefShammout123@gmail.com>
2025-10-09 15:12:55 -0400

    todays first recap was number of islands

---
+++ daily_recap.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows,cols = len(grid), len(grid[0])
+        count = 0
+
+        def dfs(x,y):
+            if x < 0 or x >= rows or y < 0 or y >= cols or grid[x][y] == "0":
+                return 
+            grid[x][y] = "0"
+
+            dfs(x + 1, y)
+            dfs(x - 1, y)
+            dfs(x, y + 1)
+            dfs(x, y - 1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    dfs(r,c)
+                    count += 1
+        return count
