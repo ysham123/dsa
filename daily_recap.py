c9a5c27707097f66205970e8fc1e6f4178d63d38
Yosef Shammout <YosefShammout123@gmail.com>
2025-10-10 15:28:25 -0400

    course schedule daily_recap

--- daily_recap.py
+++ daily_recap.py
@@ -83,3 +83,30 @@ class Solution:
             if len(maxheap) > k:
                 heapq.heappop(maxheap)
         return [[x, y] for (_, x,y) in maxheap]
+
+class Solution():
+    def canFinish(self, numCourses: int, prerequisites:List[List[int]]) -> bool:
+        graph = {i: [] for i in numCourses}
+        for a, b in prerequisites:
+            graph[b].append(a)
+
+        visited = [0] * numCourses
+        def dfs(course):
+            if visited(course) == 1:
+                return False
+            if visited(course) == 2:
+                return True
+            
+            visited[course] = 1
+
+            for neighbor in graph[course]:
+                if not dfs(neighbor):
+                    return False
+            visited[course] = 2
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True 
+        #time: O(V+E), space: O(V+ E)
