c14fc7d75dafddebd5ab1912649bbac4b089eea0
Yosef Shammout <YosefShammout123@gmail.com>
2025-10-09 23:56:01 -0400

    fixed the heappush syntax issue

--- k_closest_points_to_origin.py
+++ k_closest_points_to_origin.py
@@ -1,11 +1,11 @@
 class Solution:
-    def kclosest(self, points:List[List[int]], k: int) -> List[List[int]]:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         maxheap = []
-
+        
         for x,y in points:
-            dist = -(x*x + y * y)
-            heapq.heapq(maxheap, (dist, x, y))
+            dist = -(x*x + y*y)
+            heapq.heappush(maxheap, (dist, x, y))
 
             if len(maxheap) > k:
                 heapq.heappop(maxheap)
-            return [[x,y] for (_, x,y) in maxheap]
+        return [[x, y] for (_, x,y) in maxheap]
