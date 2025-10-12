6af600e3579e7b8194c5d4ccf638041b63ce4389
Yosef Shammout <YosefShammout123@gmail.com>
2025-10-12 17:15:07 -0400

    pivot index

---
+++ pivot_index.py
@@ -0,0 +1,11 @@
+class Solution:
+    def pivotIndex(self, nums):
+        left_sum = 0
+        total_sum = sum(nums)
+
+        for i in range(len(nums)):
+            right_sum = total_sum - left_sum - nums[i]
+            if left_sum == right_sum:
+                return i
+            left_sum += nums[i]
+        return -1
