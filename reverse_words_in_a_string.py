75806b4bca69edcb4e439061121316a6ae826924
Yosef Shammout <YosefShammout123@gmail.com>
2025-10-13 15:07:00 -0400

    another optimal solition for reverse_words_in_a_string.py

--- reverse_words_in_a_string.py
+++ reverse_words_in_a_string.py
@@ -1,3 +1,9 @@
 class Solution(object):
     def reverseWords(self, s):
-        return " ".join(reversed(s.split()))
+        """return " ".join(reversed(s.split()))     ---fully optimal""" 
+
+        #also optimal
+        words = s.split()
+        reversed_words = words[::-1]
+        reversed_string = " ".join(reversed_words)
+        return reversed_string 
