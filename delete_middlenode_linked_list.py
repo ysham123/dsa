3841b93cbfcbe06cb4e50a219438a17fc4c5918b
Yosef Shammout <YosefShammout123@gmail.com>
2025-10-18 21:39:35 -0400

    delete middlenode linked list

---
+++ delete_middlenode_linked_list.py
@@ -0,0 +1,16 @@
+class Sol(object):
+    def deleteMiddle(self, head):
+        slow, fast = head, head
+        prev = None
+
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+        if prev is None:
+            head = head.next
+        else:
+            prev.next = slow.next
+        return head
+
+        #T:O(n),S:O(1)
